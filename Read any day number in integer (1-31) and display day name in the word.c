c0573ca084712fbe10b78a5ec32b665e294beedc
MD. AL AMIN <111236323+alaminXpro@users.noreply.github.com>
2022-12-28 19:11:13 +0600

    Update Read any day number in integer (1-31) and display day name in the word.c

--- Read any day number in integer (1-31) and display day name in the word.c
+++ Read any day number in integer (1-31) and display day name in the word.c
@@ -6,7 +6,7 @@ int main(){
     scanf("%d",&day);
     if(day>31){ printf("Day is invalid!\n"); return 0;}
     count=day%7;
-    //if you did't understand, comment out the 9th line.
+    //if you did't understand, comment out the 10th line.
     //printf("Equivalent day: %d\n", count);
     switch(count){
         case 0:
