e083c963a103260c82dc9dc5a9f4c7e314b95a14
MD. AL AMIN <111236323+alaminXpro@users.noreply.github.com>
2022-12-28 19:04:13 +0600

    Add files via upload
    Write a program to read any day number in an integer (1-31) and display the day name in the word. Assume that the first day of the month is Saturday.

---
+++ Read any day number in integer (1-31) and display day name in the word.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+//alaminxpro - Previewon.com
+int main(){
+    int day,count;
+    printf("Input day: ");
+    scanf("%d",&day);
+    if(day>31){ printf("Day is invalid!\n"); return 0;}
+    count=day%7;
+    //if you did't understand, comment out the 9th line.
+    //printf("Equivalent day: %d\n", count);
+    switch(count){
+        case 0:
+            printf("Friday");
+            break;
+        case 1:
+            printf("Saturday");
+            break;
+        case 2:
+            printf("Sunday");
+            break;
+        case 3:
+            printf("Monday");
+            break;
+        case 4:
+            printf("Tuesday");
+            break;
+        case 5:
+            printf("Wednesday");
+            break;
+        case 6:
+            printf("Thursday");
+            break;
+    }
+    return 0;
+}
